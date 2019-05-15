30e13a36353d1ac5e357ab32b72d8826f5264503
Dvir Barzilay <dvir.barzilay@gmail.com>
2019-05-15 10:32:35 +0300

    helpers methods to the land mission, redesign the SubEngine class, and update the assignment instructuions file

--- src/Spacecraft.java
+++ src/Spacecraft.java
@@ -1,9 +1,12 @@
 
 public class Spacecraft {	
-	private final int NET_WEIGHT = 164;
-	private final int ANGULAR_VELOCITY = 1;
+	private final int NET_WEIGHT = 165;
+	private final int ANGULAR_ACCELERATION = 1; // angle per second^2
+	private final int MAIN_ENGINE_THRUST = 430; // N
 	private final double FUEL_BURNING_RATE = 0.2; // kg fuel per second
-	private final double INITIAL_AXIS_Z_ANGLE = 180; //Relative to the surface of the moon
+	//Relative to the surface of the moon,
+	//always at least 90 degrees. 
+	private final double INITIAL_AXIS_Z_ANGLE = 180;
 	private final double INITIAL_ALTITUDE = 30_000;
 	
 	private double fuelWeight = 216.06;
@@ -11,7 +14,10 @@ public class Spacecraft {
 	private double verticalVelocity = 43;
 	private double driveAngle = 270;
 	private double tiltAngle = 0;
-	private double axisZAngle = INITIAL_AXIS_Z_ANGLE; //Relative to the surface of the moon
+	//Relative to the surface of the moon,
+	//always at least 90 degrees. 
+	private double axisZAngle = INITIAL_AXIS_Z_ANGLE;
+	private boolean availableOrientedDirection = true;
 	private double altitude = INITIAL_ALTITUDE;
 	private double accX = 0;
 	private double accY = 0;
@@ -21,7 +27,7 @@ public class Spacecraft {
 	public Spacecraft() {
 		this.subEngines = new SubEngine[8];
 		for(int i = 0; i < 8; i++) {
-			this.subEngines[i] = new SubEngine(45*i, ANGULAR_VELOCITY);
+			this.subEngines[i] = new SubEngine(45*i, ANGULAR_ACCELERATION);
 		}
 	}
 	
@@ -29,10 +35,64 @@ public class Spacecraft {
 		return false;
 	}
 	
-	private double updatedZOrientation() {
+	/**
+	 * Update the up to date acceleration on the Z axis,
+	 * depends on the total thrust engines.
+	 */
+	private void updateAccZ() {
+		int totalThrust = MAIN_ENGINE_THRUST;
+		for(int i = 0; i < 8; i++)
+			if(subEngines[i].isOn())
+				totalThrust += subEngines[i].getEngineThrust();
+		this.accZ = totalThrust/(NET_WEIGHT+fuelWeight);
+	}
+	
+	/**
+	 * Update the angle of the spacecraft relative to the surface of the moon.
+	 * The angle of Z axis determine the orientation of the spacecraft.
+	 * @param t time passed from the last Z orientation update in seconds
+	 */
+	private void updateZOrientation(int t) {
+		for(int i = 0; i < 8; i++)
+			subEngines[i].setAngularVelocity(t, subEngines[(i+4)%8]);
+		//== this.axisZAngle += subEngines[90/45].getAngularVelocity()*t;
+		this.axisZAngle -= subEngines[270/45].getAngularVelocity()*t;
+		if(availableOrientedDirection)
+			setAvailableOrientedDirection(false);
+		double ratio = 1;
 		if(this.altitude > 1_000)
-			return ((INITIAL_AXIS_Z_ANGLE-90)/(INITIAL_ALTITUDE-1_000))*(this.altitude-1_000) +90;
-		else return 90;
+			ratio = (INITIAL_ALTITUDE-this.altitude)/(INITIAL_ALTITUDE-1_000);
+		double goalAxisZAngle = INITIAL_AXIS_Z_ANGLE - (INITIAL_AXIS_Z_ANGLE-90)*ratio*ratio;
+		if(goalAxisZAngle < this.axisZAngle) {
+				setAvailableOrientedDirection(true);
+				subEngines[270/45].setOn(true);
+		} else if(goalAxisZAngle > this.axisZAngle) {
+				setAvailableOrientedDirection(true);
+				subEngines[90/45].setOn(true);
+		}
+	}
+	
+	/**
+	 * Allows you to change the availability of changing the orientation of the spacecraft.
+	 * @param available true if you want to enable the availability to change orientation, and false otherwise.
+	 */
+	private void setAvailableOrientedDirection(boolean available) {
+		if(available)
+			for(int i = 0; i < 8; i++)
+				subEngines[i].setOn(false);
+		else
+			for(int i = 0; i < 8; i++)
+				subEngines[i].setOn(true);
+		availableOrientedDirection = available;
+	}
+	
+	/**
+	 * Update the weight of the fuel given the time passed
+	 * from the last fuel weight update in seconds.
+	 * @param t time passed from the last fuel update in seconds
+	 */
+	private void updateFuelWeight(int t) {
+		this.fuelWeight = this.fuelWeight - FUEL_BURNING_RATE*t;
 	}
 
 	public double getFuelWeight() {
--- src/SubEngine.java
+++ src/SubEngine.java
@@ -1,18 +1,50 @@
 
 public class SubEngine {
+	private final int ENGINE_THRUST = 25; // N
 	private double tiltAngle; //In the surface of the center of gravity of the spacecraft
-	private double angularVelocity; //Angle per second
-	
-	public SubEngine(double tiltAngle, double angularVelocity) {
+	private double angularAcceleration; // angle per second^2
+	private double angularVelocity = 0; // angle per second
+	private boolean on = false;
+
+	public SubEngine(double tiltAngle, double angularAcceleration) {
 		this.tiltAngle = tiltAngle;
-		this.angularVelocity = angularVelocity;
+		this.angularAcceleration = angularAcceleration;
 	}
 	
 	public double getTiltAngle() {
 		return this.tiltAngle;
 	}
 	
-	public double getAngularVelocity() {
-		return this.angularVelocity;
+	public double getAngularAcceleration() {
+		return this.angularAcceleration;
 	}
+	
+	public double getAngularVelocity() {
+		return angularVelocity;
+	}
+
+	/**
+	 * Update the angular velocity of *this specific sub-engine only*, dependent on the contrary sub-engine.
+	 * @param t delta time, since the last update.
+	 * @param contraryEngine the sub-engine that might exert an opposing force.
+	 */
+	public void setAngularVelocity(int t, SubEngine contraryEngine) {
+		if(this.isOn() && !contraryEngine.isOn())
+			this.angularVelocity += this.angularAcceleration*t;			
+		else if(!this.isOn() && contraryEngine.isOn())
+			this.angularVelocity -= contraryEngine.angularAcceleration*t;
+	}
+	
+	public boolean isOn() {
+		return on;
+	}
+
+	public void setOn(boolean on) {
+		this.on = on;
+	}
+
+	public int getEngineThrust() {
+		return ENGINE_THRUST;
+	}
+	
 }
