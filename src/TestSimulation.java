04f8789ba70bcb0275a79f9501a6a0831fa50cc3
Dvir Barzilay <dvir.barzilay@gmail.com>
2019-05-27 09:44:41 +0300

    A crash investigation of Beresheet.docx

--- src/TestSimulation.java
+++ src/TestSimulation.java
@@ -4,7 +4,7 @@ public class TestSimulation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Beresheet2 beresheet = new Beresheet2();
+		Beresheet3 beresheet = new Beresheet3();
 		beresheet.land();
 	}
 
